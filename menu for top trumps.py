8cd5083731340b17d0872d161df37230e0b18ec4
nswids <swidler.n@etoncollege.org.uk>
2016-05-12 09:42:21 +0100

    Add files via upload
    Updated the menu, and added the instructions so that it works when they choose that number.

--- menu for top trumps.py
+++ menu for top trumps.py
@@ -9,12 +9,45 @@ def gameMenu():
           print("Hello ", name, ". Let's play Top Trumps!")
           playGame()
         elif ans=="2":
-          print("\n Okay then: here are the instructions: ")
-          print(gameInstruction)
+          print("\n Okay then. ")
+          instructions()
+          askQuestion = input("do you want to play the game now?")
+          if askQuestion == 'yes':
+              print("Okay. Let's play!")
+              playGame()
         elif ans=="3":
           print("\n Goodbye") 
           ans = False
         else:
-           print("\n Please Enter an Option Shown")
+            print("\n Please Enter an Option Shown")
+
+def instructions():
+    listInstruction = print(""" \n Here are the Instructions for the game:
+
+              1.You are given five top trumps, as does the computer
+
+              2.List the category you want to pick
+
+              3.If the category you picked has more points than the computer's,
+              then you get a point.
+
+              4.If you have the highest number after five rounds, then you win!
+
+              """)
+    
+
+    
+    
 
 
+
+ 
+   
+
+
+gameMenu()
+
+
+
+
+    
