529a1a32ea46d165acecfe1b9934edcb30e13077
nswids <swidler.n@etoncollege.org.uk>
2016-05-16 11:21:33 +0100

    Add files via upload
    have started coding the grid, but I still need to tweak it a bit

---
+++ minsweep grid.py
@@ -0,0 +1,19 @@
+def makeGrid():
+    letterArray = ['O']
+    return letterArray
+
+    counter = 0
+    for i in range(0,9):
+        for x in range(0,9):
+            letterArray.append('O')
+            print(letArray[counter], " ",end="")
+            counter = counter + 1
+        print('\n')
+
+def main():
+    anyGrid = makeGrid()
+    print(anyGrid)
+
+
+main()
+    
