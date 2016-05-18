4cdda7446c10751fabe37efceff959ee219c2d6a
nswids <swidler.n@etoncollege.org.uk>
2016-05-18 21:09:45 +0100

    bomb grid first try/attempt
    Adrien, I have tried to make a second board but am unsure on what you want me to do. am also confused- but my code does not make sense, whaat i have tried to do is make another grid but print, randomly, the bombs onto the grid

--- make board 2.py
+++ make board 2.py
@@ -1,14 +1,34 @@
+import random
+
 def showGrid():
     board = []
     for i in range(9):
         board.append([])
         for x in range(9):
             board[i].append('+')
-
     for row in board:
         print (" ".join(row))
 
-showGrid()
+def bombBoard():
+    bombGrid = []
+    for i in range(9):
+        bombGrid.append([])
+        for x in range(9):
+            bombGrid[i].append('@')
+            bombGrid[i].append('+')
+            bombGrid[i].append('+')
+            bombGrid[i].append('+')
+            randGrid = print(random.choice(bombGrid))
+            randGrid()
+    for row in bombGrid:
+        print("".join(row))
+
+bombBoard()
+        
+        
+        
+
+
 
 
             
